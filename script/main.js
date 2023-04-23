8705bb46aec7361ad81aa4c99acf630d78b6ae11
emilaynerodrigues <emilaynx@gmail.com>
2023-04-23 18:02:43 -0400

    adicionando novos svgs, animações e um card com texto de parabens

--- script/main.js
+++ script/main.js
@@ -3,8 +3,6 @@ var minutesContainer = document.querySelector("#minute");
 var hoursContainer = document.querySelector("#hour");
 var daysContainer = document.querySelector("#day");
 var ageContainer = document.querySelector("#age");
-var countdownContainer = document.querySelector("#countdown");
-var congratsContainer = document.querySelector("#congrats");
 var moonContainer = document.querySelector("#moon");
 
 function countdown() {
@@ -39,12 +37,10 @@ function countdown() {
     var currentDate = new Date();
     if (currentDate.getMonth() == 3 && currentDate.getDate() == 25) {
       // Se for 25 de abril, esconde o countdown e exibe a mensagem + botões
-      // countdownContainer.classList.add("hidden");
       document.querySelector("#countdown").style.display = "none";
-      document.querySelector("#congrats").style.display = "flex"; 
+      document.querySelector("#congrats").style.display = "flex";
     } else {
       // // Se não for 25 de abril, mostra o countdown e esconde a mensagem + botões
-      // countdownContainer.classList.remove("hidden");
       document.querySelector("#countdown").style.display = "flex";
       document.querySelector("#congrats").style.display = "none";
     }
@@ -78,16 +74,89 @@ window.onload = function () {
   countdown();
 };
 
-function btnPress() {
-  alert("Ops. Se esforce mais! 😛");
-}
+// Função que atribui uma função aos botões de forma aleatoria e sempre que usuário
+//clicar no botão correto (que está recebendo a função no momento), será exibido a animação
 
-function blueBtnPress() {
+var buttons = document.querySelectorAll(".button");
+var correctButtonIndex = Math.floor(Math.random() * buttons.length);
+var correctButton = buttons[correctButtonIndex];
+
+correctButton.addEventListener("click", function () {
   document.querySelector("#congrats").style.display = "none";
   document.querySelector("#congrats").style.visibility = "hidden";
   document.querySelector("#effects").style.display = "flex";
-  document.querySelector("#shooting-stars").style.display = "flex";
-  document.querySelector("#moon-floor").style.display = "flex";
   moonContainer.classList.add("moon-transition");
-  
+
+  function shootingStars() {
+    document.querySelector("#shooting-stars").style.display = "flex";
+  }
+
+  setTimeout(shootingStars, 5000);
+});
+
+for (let i = 0; i < buttons.length; i++) {
+  if (i !== correctButtonIndex) {
+    buttons[i].addEventListener("click", function () {
+      alert("Ops. Se esforce mais! 😛");
+    });
+  }
 }
+
+// função que mostra os efeitos extras e o card quando o ovni é clicado
+var ovni = document.querySelector("#ovni");
+var balloons = document.querySelector(".balloons");
+var head = document.querySelector(".head");
+
+function ovniBtn() {
+  // alert("oi");
+  ovni.classList.add("ovni-effect");
+  document.querySelector(".arrow").style.display = "none";
+  balloons.classList.add("balloons-up");
+  head.classList.add("head-up");
+
+  function showCard() {
+    document.querySelector(".card").style.display = "flex";
+  }
+
+  setTimeout(showCard, 4000);
+
+  function stopShootingStars() {
+    document.querySelector("#shooting-stars").style.opacity = "0";
+  }
+
+  setTimeout(stopShootingStars, 4200);
+}
+
+// configuração do typeit js
+
+new TypeIt(".text", {
+  speed: 40,
+  waitUntilVisible: true,
+})
+  .pause(1000)
+  .type("Oi, oi, testando.. Parece que tá funcionando. Valos", { delay: 300 })
+  .move(-2)
+  .delete(1)
+  .type("m")
+  .move(null, { to: "END" })
+  .type(" lá!")
+  .pause(1000)
+  .delete(55)
+  .type(
+    "O universo esperou por você e recebeu a dádiva de sua presença há exatos 18 anos, mas ele ainda não conhece sua capacidade. Ele ainda está tentando desvendar o mistério por trás da pessoa que você é e irá se tornar. <br><br>Você trouxe luz para o espaço e ilumina a vida daqueles que estão ao seu redor. Voçê traz alegria, inspiração e deixa queixos caídos por onde passa, mas não por simplesmente ter sido contemplada com tanta beleza. <br><br>"
+  )
+  .type(
+    "Você é o tipo de pessoa fácil de apaixonar e eu poderia listar diversas das suas melhores qualidades, MAS NÃO TEM MAIS ESPAÇOOOOOOOOOOOOOOOOOOO. Vou ter que apagar esse :("
+  )
+  .pause(3000)
+  .delete(171)
+  .type(
+    "Resumindo: você é especial e só existe você sendo você. E caramba, eu poderia falar bem mais sobre você, mas nem tudo é perfeito, sabe? E isso não tá ficando legal"
+  )
+  .pause(3000)
+  .delete(108)
+  .type("<br><br>Feliz aniversario, Barbára!")
+  .pause(1000)
+  .delete(7)
+  .type("árbara!")
+  .go();
